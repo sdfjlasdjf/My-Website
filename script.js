15705da271757fe0965132552492169e51579228
Jiangshan Ai <53445198+sdfjlasdjf@users.noreply.github.com>
2025-02-02 00:04:16 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let count = 0;
-const counterBtn = document.getElementById('counterBtn');
-const counterDisplay = document.getElementById('counterDisplay');
-
-counterBtn.addEventListener('click', () => {
-    count++;
-    counterDisplay.textContent = `This button has been clicked ${count} times.`;
-});
